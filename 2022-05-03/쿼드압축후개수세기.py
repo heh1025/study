71d02af4de1aeca3a6a8bc0c41ea5427c79c06be
heh1025 <hwangeh1025@naver.com>
2022-05-03 18:08:28 +0900

    Update 쿼드압축후개수세기.py

--- 2022-05-03/쿼드압축후개수세기.py
+++ 2022-05-03/쿼드압축후개수세기.py
@@ -4,6 +4,8 @@ def solution(arr):
         return [0, 1]
     elif sum([sum(i) for i in arr]) == 0:
         return [1, 0]
+
+
     else:
         return [sum(i) for i in zip(*[solution([i[:h] for i in arr[:h]]),
                                       solution([i[h:] for i in arr[:h]]),
